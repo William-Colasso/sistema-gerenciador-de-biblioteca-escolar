5f5a025a09babe2a64c3c499049a78a711b0db6c
VINICIUS PEDRO ANDREAZZA <vinicius_andreazza@estudante.sesisenai.org.br>
2025-11-26 16:43:13 -0300

    add: estrutura inicial do projeto

---
+++ sistema-gerenciador-biblioteca-escolar/src/main/java/com/aati/sgbe/sistema_gerenciador_biblioteca_escolar/SistemaGerenciadorBibliotecaEscolarApplication.java
@@ -0,0 +1,20 @@
+package com.aati.sgbe.sistema_gerenciador_biblioteca_escolar;
+
+import javax.swing.UnsupportedLookAndFeelException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciadorBibliotecaEscolarApplication {
+
+ 
+
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
+
+        
+        SpringApplication.run(SistemaGerenciadorBibliotecaEscolarApplication.class, args);
+    	
+    }
+
+}
---
+++ sistema-gerenciador-biblioteca-escolar/src/test/java/com/aati/sgbe/sistema_gerenciador_biblioteca_escolar/SistemaGerenciadorBibliotecaEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aati.sgbe.sistema_gerenciador_biblioteca_escolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciadorBibliotecaEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
