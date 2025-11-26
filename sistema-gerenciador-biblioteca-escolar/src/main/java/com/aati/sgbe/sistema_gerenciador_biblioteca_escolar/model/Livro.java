091f96dad087239406978deb20ea1b0331e48e0b
LUCIANO AUGUSTO ZUMACH <luciano_zumach@estudante.sesisenai.org.br>
2025-11-26 16:46:20 -0300

    add: feature de cadastro de livros

---
+++ sistema-gerenciador-biblioteca-escolar/src/main/java/com/aati/sgbe/sistema_gerenciador_biblioteca_escolar/model/Livro.java
@@ -0,0 +1,84 @@
+package com.aati.sgbe.sistema_gerenciador_biblioteca_escolar.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    private String autor;
+
+    private String genero;
+
+    private String editora;
+
+    private Date ano;
+
+    private int quantidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Date getAno() {
+        return ano;
+    }
+
+    public void setAno(Date ano) {
+        this.ano = ano;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+}
