3d2c2c9c2889093f90c114cbdfc731fa24cab29c
VINICIUS PEDRO ANDREAZZA <vinicius_andreazza@estudante.sesisenai.org.br>
2025-11-26 16:55:59 -0300

    add: funções de administração e gerenciamento

---
+++ sistema-gerenciador-biblioteca-escolar/src/main/java/com/aati/sgbe/sistema_gerenciador_biblioteca_escolar/service/UsuarioService.java
@@ -0,0 +1,35 @@
+package com.aati.sgbe.sistema_gerenciador_biblioteca_escolar.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aati.sgbe.sistema_gerenciador_biblioteca_escolar.model.Usuario;
+import com.aati.sgbe.sistema_gerenciador_biblioteca_escolar.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public Usuario validarLogin(String nome, String senha) {
+        Usuario user = usuarioRepository.findByNome(nome);
+        if (user != null && user.getSenha().equals(senha)) {
+            return user;
+        }
+        return null;
+    }
+
+    public void create(Usuario usuario){
+        usuarioRepository.save(usuario);
+    }
+
+    public Usuario getById(Long id){
+        return usuarioRepository.findById(id).get();
+    }
+
+    public List<Usuario> getAll(){
+        return usuarioRepository.findAll();
+    }
+}
